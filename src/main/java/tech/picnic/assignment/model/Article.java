7084aa223be64fb7dde8382b1b199322951294a0
Khalil SLEIMI <khalil.swdp+oss.github@gmail.com>
2022-06-08 22:06:17 +0100

    Add method to class to modify inner property

--- src/main/java/tech/picnic/assignment/model/Article.java
+++ src/main/java/tech/picnic/assignment/model/Article.java
@@ -11,4 +11,9 @@ public class Article {
     private String id;
     private String name;
     private String temperatureZone;
+
+    // method to change the name of the article to upper case
+    public void makeNameUpperCase() {
+        this.name = name.toUpperCase();
+    }
 }
