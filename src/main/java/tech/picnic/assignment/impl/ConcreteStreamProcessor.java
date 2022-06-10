e88d57948cbbfebb52dbf6c37ee97e202ed975d9
Khalil SLEIMI <khalil.swdp+oss.github@gmail.com>
2022-06-10 18:28:46 +0100

    Implement the missing pieces (reading from json, filtering, sorting, grouping and finally writing to sink)

--- src/main/java/tech/picnic/assignment/impl/ConcreteStreamProcessor.java
+++ src/main/java/tech/picnic/assignment/impl/ConcreteStreamProcessor.java
@@ -1,15 +1,18 @@
 package tech.picnic.assignment.impl;
 
 import tech.picnic.assignment.api.StreamProcessor;
-import tech.picnic.assignment.model.Article;
+import tech.picnic.assignment.model.ArticleProcessedInput;
 import tech.picnic.assignment.model.Pick;
 import tech.picnic.assignment.model.Picker;
+import tech.picnic.assignment.model.PickerProcessedInput;
 
 import java.io.*;
 import java.time.Duration;
 import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
+
+import static tech.picnic.assignment.model.PickerProcessedInput.toJson;
 
 public class ConcreteStreamProcessor implements StreamProcessor {
 
@@ -40,20 +43,52 @@ public class ConcreteStreamProcessor implements StreamProcessor {
                 continue;
             }
             // Unmarshal/Deserialize the JSON. and create a Pick object. and add it to the picks list
-            // Pick pick = Pick.fromJson(line); ? (make each object have code responsible for marshalling its properties)
-            Pick pick = new Pick();
-            picks.add(pick);
+            try {
+                Pick pick = Pick.fromJson(line);
+                picks.add(pick);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
 
         }
+        reader.close();
 
+
+        // Filter and Make Article Name Upper Case
         Stream<Pick> picksStream = picks.stream()
-                .filter(pick -> pick.getArticle().getTemperatureZone().equals("ambient"));
-        picksStream.forEach(pick -> pick.getArticle().makeNameUpperCase());
+                .filter(pick -> pick.getArticle().getTemperature_zone().equals("ambient"))
+                .peek(pick -> pick.getArticle().makeNameUpperCase());
+
+        // Group by Picker
         Map<Picker, List<Pick>> groupedPicksByPicker = picksStream.collect(Collectors.groupingBy(Pick::getPicker));
+
+        // Sort Picks by Timestamp
         groupedPicksByPicker.forEach((id, picksByPicker) -> picksByPicker.sort(Comparator.comparing(Pick::getTimestamp)));
+
+        // The Pickers must be sorted chronologically (ascending) by their active_since timestamp, breaking ties by ID.
+        List<Picker> sortedPickers = groupedPicksByPicker.keySet().stream()
+                .sorted(Comparator.comparing(Picker::getActive_since).thenComparing(Picker::getId))
+                .collect(Collectors.toList());
+
+        // Map the previous into a list of processedinput
+        List<PickerProcessedInput> pickerProcessedInputs = new ArrayList<>();
+        for (Picker picker : sortedPickers) {
+            List<Pick> picksByPicker = groupedPicksByPicker.get(picker);
+            pickerProcessedInputs.add(
+                    new PickerProcessedInput(
+                            picker.getName(),
+                            picker.getActive_since(),
+                            picksByPicker.stream().map(
+                                    pick -> new ArticleProcessedInput(
+                                            pick.getArticle().getName(),
+                                            pick.getTimestamp())).collect(Collectors.toList())));
+        }
+        
+        
         // Marshal/Serialize the groupedPicksByPicker to JSON, and write it to the sink, according to the given standard
         // Or introduce our own class to represent the output. And to be filled with the data from the groupedPicksByPicker
-
+        // and the sortedPickers.
+        toJson(sink, pickerProcessedInputs);
 
         close();
     }
